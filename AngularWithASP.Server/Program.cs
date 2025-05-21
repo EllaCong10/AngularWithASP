f75c71abdde68f1ce3f500eb9e16e77c5fff2f95
Ella Cong <conge@legal.regn.net>
2025-05-21 15:36:16 +0800

    Add static icons

--- AngularWithASP.Server/Program.cs
+++ AngularWithASP.Server/Program.cs
@@ -2,15 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(); // 注册控制器服务
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+
+// 用于托管 Angular 静态文件
+app.UseDefaultFiles();// 查找 index.html
+app.UseStaticFiles();// 默认只处理 wwwroot 下文件
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,7 +26,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+// 支持 Angular 路由
 app.MapFallbackToFile("/index.html");
 
 app.Run();
